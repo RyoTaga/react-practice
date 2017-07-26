460e999606c292b9329e7308574a6dc8ca1da2fe
Ryo Taga <taga@n-di.co.jp>
2017-07-26 17:41:36 +0900

    change function to class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const App = () => <h1>Hello React</h1>
+class App extends React.Component {
+  render() {
+    return React.createElement('h1', null, 'Hello React')
+  }
+}
 
 export default App
